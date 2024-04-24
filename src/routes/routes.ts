1c8afd0ac004f199cd1e21d8828e98daf73a9639
Davi <lobpreal@gmail.com>
2024-04-23 22:48:29 -0300

    :sparkles: feat: added route login in file routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import { UserController } from '../controllers/userController';
 import { loggedInUser } from '../middlewares/loggedInUser';
 import { validateBodyRequest } from '../schema/validateBody';
-import { schemaRegister } from '../validations/validateBodyUser';
+import { schemaLogin, schemaRegister } from '../validations/validateBodyUser';
 
 const routes: Router = Router();
 
 const userController: UserController = new UserController();
 
+routes.post('auth/login', validateBodyRequest(schemaLogin), userController.login);
 routes.post('auth/register', validateBodyRequest(schemaRegister), userController.store);
 routes.get('/user', loggedInUser, userController.index);
 routes.get('/user/:id', loggedInUser, userController.show);
